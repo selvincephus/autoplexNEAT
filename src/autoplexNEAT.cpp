465dc1e233bcaab8ad8534b91ef7b86bf92cd497
selvincephus <selvincephus@gmail.com>
2016-09-12 18:16:32 +0200

    Testing commits to git

--- src/autoplexNEAT.cpp
+++ src/autoplexNEAT.cpp
@@ -1,6 +1,7 @@
 /**autoplexNEAT - Autonomous classification system for 
 		  complex test scenarios using NEAT 
-		  realized with OpenDaVINCI.
+		  realized with OpenDaVINCI. Code available
+		  on github : selvincephus/autoplexNEAT.
 		  12/09/2016 - Selvin Cephus Jayakumar **/
 
 #include <iostream>
