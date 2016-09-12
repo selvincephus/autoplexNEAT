559d63e6d4a2a01c27ac45c0648506958ee83fea
selvincephus <selvincephus@gmail.com>
2016-09-12 17:50:07 +0200

    First commit

---
+++ src/autoplexNEAT.cpp
@@ -0,0 +1,13 @@
+/**autoplexNEAT - Autonomous classification system for 
+		  complex test scenarios using NEAT 
+		  realized with OpenDaVINCI.
+		  12/09/2016 - Selvin Cephus Jayakumar **/
+
+#include <iostream>
+
+#include "opendavinci/odcore/data/Container.h"
+
+#include "opendavinci/GeneratedHeaders_OpenDaVINCI.h"
+#include "automotivedata/GeneratedHeaders_AutomotiveData.h"
+
+#include "autoplexNEAT.h"
